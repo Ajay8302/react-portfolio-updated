2f3a612b4d72622863af21c4dd061aa992b40522
Ajay8302 <ajay.ak8302@com.com>
2025-08-17 15:16:09 +0530

    Initial commit of React portfolio

---
+++ src/constants/data.ts
@@ -0,0 +1,553 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "Skills",
+    href: "#skills",
+  },
+  {
+    id: 3,
+    name: "Experience",
+    href: "#experience",
+  },
+  {
+    id: 4,
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "firstCommerce – Scalable Web Application for Online Shopping (Inspired by Amazon)",
+    desc: "firstCommerce is a all-sreens compatible e-commerce platform inspired by Amazon, offering product browsing, cart management, and secure checkout with real-time updates.",
+    subdesc:
+      "This is a modern e-commerce app built with React and Firebase, firestore, Redux and React Router.",
+    href: "https://firstcommerce-dev.netlify.app/",
+    source: "https://github.com/Ajay8302/firstcommerce",
+    logo: "projects/firstcommerce.png",
+    logoStyle: {
+      backgroundColor: "#b18eff",
+      background:
+        "linear-gradient(0deg, #B18EFF50, #B18EFF50), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(177, 142, 255, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(177, 142, 255, 0.3)",
+    },
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "icons/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "icons/tailwindcss-icon.svg",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "icons/typescript-icon.svg",
+      },
+      {
+        id: 4,
+        name: "Java Script",
+        path: "icons/javascript.svg",
+      },
+      {
+        id: 5,
+        name: "Firebase",
+        path: "icons/firebase.svg",
+      },
+      {
+        id: 6,
+        name: "HTML",
+        path: "icons/html-5.svg",
+      },
+      {
+        id: 7,
+        name: "Redux",
+        path: "icons/redux.svg",
+      },
+      {
+        id: 8,
+        name: "Prettier",
+        path: "icons/prettier.svg",
+      },
+      {
+        id: 9,
+        name: "ESLint",
+        path: "icons/eslint.svg",
+      },
+      {
+        id: 10,
+        name: "Vite",
+        path: "icons/vite.svg",
+      },
+      {
+        id: 11,
+        name: "FireStore",
+        path: "icons/firestore.svg",
+      },
+    ],
+  },
+  {
+    title: "Ajay Kumar - Portfolio Website",
+    desc: "My Portfolio Website, a showcase of my skills, projects, and experience as a software developer.",
+    subdesc:
+      "Build with cutting-edge web technologies, this portfolio showcases dynamic 3D components, fluid animations, and adaptive design for a captivating UX.",
+    href: "https://github.com/#/portfolio-website",
+    source: "https://portfolio-website/",
+    logo: "projects/Portfolio_Main.png",
+    logoStyle: {
+      backgroundColor: "#4FC3F7",
+      background:
+        "linear-gradient(0deg, #4FC3F750, #4FC3F750), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(79, 195, 247, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(79, 195, 247, 0.3)",
+    },
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "icons/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "icons/tailwindcss-icon.svg",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "icons/typescript-icon.svg",
+      },
+      {
+        id: 4,
+        name: "Java Script",
+        path: "icons/javascript.svg",
+      },
+      {
+        id: 5,
+        name: "Three.js",
+        path: "icons/threejs.svg",
+      },
+      {
+        id: 6,
+        name: "HTML",
+        path: "icons/html-5.svg",
+      },
+      {
+        id: 7,
+        name: "CSS",
+        path: "icons/css-3.svg",
+      },
+      {
+        id: 8,
+        name: "Prettier",
+        path: "icons/prettier.svg",
+      },
+      {
+        id: 9,
+        name: "ESLint",
+        path: "icons/eslint.svg",
+      },
+      {
+        id: 10,
+        name: "Vite",
+        path: "icons/vite.svg",
+      },
+    ],
+  },
+    {
+    "title": "shopByte - Full-Stack E-Commerce Application",
+    "desc": "A full-stack e-commerce web application built using .NET Core, Angular, and Node.js, integrating JWT authentication and Swagger for API documentation.",
+    "subdesc": "Developed with a robust tech stack, including HTML5, CSS3, Bootstrap, and advanced features such as secure authentication and a responsive design for a seamless shopping experience.",
+    "href": "#",
+    "source": "https://github.com/#",
+    "logo": "projects/shopbyte1.png",
+    "logoStyle": {
+      "backgroundColor": "#A0F0BC",
+      "background": "linear-gradient(0deg, #A0F0BC50, #A0F0BC50), linear-gradient(180deg, rgba(255, 255, 255, 0.95) 0%, rgba(208, 213, 221, 0.85) 100%)",
+      "border": "0.2px solid rgba(160, 240, 188, 1)",
+      "boxShadow": "0px 0px 60px 0px rgba(135, 234, 174, 0.4)"
+    },
+    "tags": [
+      {
+        "id": 1,
+        "name": "HTML5",
+        "path": "icons/html-5.svg"
+      },
+      {
+        "id": 2,
+        "name": "CSS3",
+        "path": "icons/css-3.svg"
+      },
+      {
+        "id": 3,
+        "name": "Bootstrap",
+        "path": "icons/bootstrap.svg"
+      },
+       {
+        "id": 4,
+        "name": ".NET Core",
+        "path": "icons/dotNet.svg"
+      },
+      {
+        "id": 5,
+        "name": "C Sharp",
+        "path": "icons/c-sharp.svg"
+
+      },
+      {
+        "id": 6,
+        "name": "Angular",
+        "path": "icons/angular.svg"
+      },
+      {
+        "id": 7,
+        "name": "Node.js",
+        "path": "icons/nodejs.svg"
+      },
+      {
+        "id": 8,
+        "name": "JWT",
+        "path": "icons/Jwt.svg"
+      }
+    ]
+  },
+  // {
+  //   title: "",
+  //   desc: "#",
+  //   subdesc:
+  //     "#",
+  //   href: "#",
+  //   source: "#",
+  //   logo: "#.png",
+  //   logoStyle: {
+  //     backgroundColor: "#FF4C4C",
+  //     background:
+  //       "linear-gradient(0deg, #FF4C4C50, #FF4C4C50), linear-gradient(180deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 200, 200, 0.85) 100%)",
+  //     border: "0.2px solid rgba(255, 76, 76, 1)",
+  //     boxShadow: "0px 0px 60px 0px rgba(183, 28, 28, 0.4)",
+  //   },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "React.js",
+  //       path: "icons/react.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Nextjs",
+  //       path: "icons/nextjs.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "TailwindCSS",
+  //       path: "icons/tailwindcss-icon.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TypeScript",
+  //       path: "icons/typescript-icon.svg",
+  //     },
+  //     {
+  //       id: 5,
+  //       name: "Java Script",
+  //       path: "icons/javascript.svg",
+  //     },
+  //     {
+  //       id: 6,
+  //       name: "HTML",
+  //       path: "icons/html-5.svg",
+  //     },
+  //     {
+  //       id: 7,
+  //       name: "CSS",
+  //       path: "icons/css-3.svg",
+  //     },
+  //     {
+  //       id: 8,
+  //       name: "Prettier",
+  //       path: "icons/prettier.svg",
+  //     },
+  //     {
+  //       id: 9,
+  //       name: "ESLint",
+  //       path: "icons/eslint.svg",
+  //     },
+  //     {
+  //       id: 10,
+  //       name: "Markdown",
+  //       path: "icons/markdown.svg",
+  //     },
+  //     {
+  //       id: 11,
+  //       name: "Vite",
+  //       path: "icons/vite.svg",
+  //     },
+  //     {
+  //       id: 12,
+  //       name: "Vercel",
+  //       path: "icons/vercel.svg",
+  //     },
+  //   ],
+  // },
+];
+
+export const workExperiences = [
+  {
+    id: 1,
+    company: "Aplty Technologies",
+    position: "Software Developement Engieer 1 (R&D)",
+    duration: "June 2025 - current - Bengaluru, India",
+    icon: "aptly_logo.png",
+    technologies: [
+      "React Js",
+      "ASP .Net Core",
+      "C#",
+      "TailwindCSS",
+      "Next Js",
+      "Bun Js",
+      "Figma",
+      "Better-Auth",
+      "Agile",
+      "AZURE DevOps",
+      "Git",
+      "GitHub",
+      "PostgresQL",
+      "Socket.io",
+      "Drizzle-ORM",
+      "Turborepo",
+      "Zudstand",
+      "Node Js",
+    ],
+    projects: [
+      {
+        name: "Aptly Star and AgentLauchpad Product Developement",
+        // description: "Aptly Star is a comprehensive platform designed to streamline the development and management of AI agents, providing tools for building, deploying, and monitoring agents across various domains.",
+        link: "",
+      },
+      {
+        name: "Microsoft - Web Application",
+        // description: "A web application developed for Microsoft, focusing on enhancing user experience and functionality.",
+        link: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    company: "Mphasis Limited",
+    position: "Associate Software Engineer",
+    duration: "Ferburary 2022 - June 2025 - Bengaluru, India",
+    icon: "mphasis_logo.png",  
+    technologies: [
+      "JavaScript",
+      "C#",
+      "C/C++",
+      "XML",
+      "Angular 8+",
+      "ASP.Net Core",
+      "Entity Framework",
+      "Design Patterns",
+      "TypeScript",
+      "X-Unit",
+      "Express.js",
+      "REST APIs",
+      "MYSQL",
+      "MicroServices",
+      "Azure",
+      "HTML5",
+      "CSS3",
+      "TailwindCSS",
+      "Redux",
+      "Vitest",
+      "Jest",
+      "React",
+      "Vite",
+      "CI/CD",
+      "Docker",
+      "Jenkins",
+      "Confluence",
+      "JIRA",
+    ],
+    projects: [
+      {
+        name: "State Bank of India - Application",
+        description: "",
+        link: "",
+      },
+      {
+        name: "H.P India - Printer Applications",
+        description: "",
+        link: "",
+      },
+      // {
+      //   name: "Supply Chain DLX",
+      //   description: "",
+      //   link: "",
+      // },
+      // {
+      //   name: "CCSIF Trucking DLX",
+      //   description: "",
+      //   link: "",
+      // },
+      // {
+      //   name: "Reacteroids Onboarding",
+      //   description: "",
+      //   link: "",
+      // },
+      // {
+      //   name: "vConestoga Platform",
+      //   description: "",
+      //   link: "",
+      // },
+      // {
+      //   name: "Trucking Inspection DLX",
+      //   description: "",
+      //   link: "",
+      // },
+      // {
+      //   name: "Motive Power DLX",
+      //   description: "",
+      //   link: "",
+      // },
+    ],
+  },
+  {
+    id: 3,
+    company: "Auribises Technologies Pvt. Ltd.",
+    position: "Software Developer Intern",
+    duration: "January 2021 - December 2021 - Punjab, India",
+    icon: "auribises_logo.png",
+    technologies: [
+      "C#",
+      ".Net Core MVC",
+      "WCF",
+      "CSS3",
+      "JavScript",
+      "Agile",
+      "Google Firebase",
+      "Google FireStore",
+      "Git",
+      "Python",
+      "Python M.L Libraries",
+    ],
+    projects: [
+      {
+        name: "Food Delivery - Web Application",
+        description: "",
+        link: "",
+      },
+      {
+        name: "Automated Car Parking System",
+        description: "",
+        link: "",
+      },
+      // {
+      //   name: "Machine Shop DLX",
+      //   description: "",
+      //   link: "",
+      // },
+      // {
+      //   name: "Bloom DLX",
+      //   description: "",
+      //   link: "",
+      // },
+    ],
+  },
+  // {
+  //   id: 3,
+  //   company: "Conestoga College",
+  //   position: "Mobile Application Developer (Co-op)",
+  //   duration: "June 2020 - December 2020",
+  //   icon: "conestoga_logo.png",
+  //   technologies: [
+  //     "Java",
+  //     "Swift",
+  //     "Python",
+  //     "Figma",
+  //     "Firebase",
+  //     "AR",
+  //     "Agile",
+  //     "Scrum",
+  //     "Git",
+  //     "GitHub",
+  //   ],
+  //   projects: [
+  //     {
+  //       name: "AR Mask Sizing Assistant",
+  //       description: "",
+  //       link: "",
+  //     },
+  //   ],
+  // },
+];
+
+export const socialLinks = [
+  { url: "https://github.com/Ajay8302" },
+  { url: "https://www.linkedin.com/in/ajaykumardev/" },
+  { url: "https://leetcode.com/u/user4754Fv/" },
+  { url: "https://x.com/ajay_Dev1" },
+  { url: "https://www.facebook.com/profile.php?id=100010283986894" }
+];
+
+export const skills = [
+  "Angular",
+  "React",
+  "REST APIs",
+  "JavaScript",
+  "TypeScript",
+  "C#",
+  "C/C++",
+  "JAVA",
+  "Machine Learning",
+  "Python",
+  "Jira",
+  "SonarCube",
+  ".Net Core MVC",
+  "HTML5",
+  "CSS3",
+  "Node.js",
+  "ASP .Net", 
+  "Express.js",
+  "Git",
+  "MongoDB",
+  "Azure",
+  "Graph SQL",
+  "Jest",
+  "MySQL",
+  "Scrum",
+  "Agile",
+  "Gen. A.I.",
+  "L.L.M's",
+  "GitHub",
+  "Confluence",
+  "MicroServices",
+  "GitLab",
+  "Redux",
+  "X-Unit",
+  "TailwindCSS",
+  "Vitest",
+  "Bitbucket",
+  "Algorithms",
+  "UI/UX",
+  "Vite",
+  "Redis",
+  "Git Bash",
+  "Vercel",
+  "CI/CD",
+  "Figma",
+  "Docker",
+  "Kubernetes",
+];
